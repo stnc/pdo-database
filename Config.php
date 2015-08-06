82bfa4c6a05d6fa2ba3c5b9bc162915bd83620c1
stnc <selmantunc@yandex.com>
2015-08-06 23:01:28 +0300

    add

--- Config.php
+++ Config.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_TYPE', 'mysql');
   define('DB_HOST', 'localhost');
-  define('DB_NAME', 'application');
+  define('DB_NAME', 'alem');
   define('DB_USER', 'root');
   define('DB_PASS', '');
 
